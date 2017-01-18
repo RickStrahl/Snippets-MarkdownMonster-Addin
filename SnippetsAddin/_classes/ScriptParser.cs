ff5dc48c3e2e45ac1c1587295c32fc3b6497778c
Rick Strahl <rstrahl@west-wind.com>
2017-01-18 00:32:13 -1000

    Remove Roslyn Script code.

--- SnippetsAddin/_classes/ScriptParser.cs
+++ SnippetsAddin/_classes/ScriptParser.cs
@@ -81,76 +81,7 @@ namespace SnippetsAddin
 
             return result;
         }
-
-        /// <summary>
-        /// Executes a script with embedded {{ C# expression }}  syntax.
-        /// Anything between double brackets is executed.
-        /// 
-        /// This async version is the preferred way to call this method.
-        /// </summary>
-        /// <param name="snippet"></param>
-        /// <param name="state"></param>
-        /// <returns></returns>
-        public async Task<string> EvaluateScriptAsync(string snippet, object state = null)
-        {
-            snippet = snippet.Replace("{{", "\" + ").Replace("}}", " + @\"");
-            snippet = "@\"" + snippet + "\"";
-
-
-            var scripting = new wwScripting();
-            string result = scripting.ExecuteCode("return " + snippet + ";" ) as string;
-
-            if (result == null)
-                ErrorMessage = scripting.ErrorMessage;
-            else
-                ErrorMessage = null;
-
-            return result;
-
-#if false
-            var options = ScriptOptions.Default;
-
-            //Add reference to mscorlib
-            var mscorlib = typeof(System.Object).Assembly;
-            var systemCore = typeof(System.Linq.Enumerable).Assembly;
-
-            try
-            {
-                options = options
-                    .AddReferences(mscorlib);
-                options = options
-                    .AddReferences(systemCore);
-                
-                options = options
-                    .AddImports("System")
-                    .AddImports("System.IO")
-                    .AddImports("System.Text")
-                    .AddImports("System.Net")
-                    .AddImports("System.Linq")
-                    .AddImports("System.Collections.Generic");
-
-
-                foreach (string ns in Namespaces)
-                {
-                    if (!options.Imports.Contains(ns))
-                        options.AddImports(ns);
-                }
-                foreach (string reference in References)
-                {
-                    options.AddReferences(reference);
-                }
-
-                //note: we block here, because we are in Main method, normally we could await as scripting APIs are async
-                return CSharpScript.EvaluateAsync<string>(snippet, options, state).Result;
-            }
-            catch (Exception ex)
-            {
-                ErrorMessage = ex.Message;
-                return null;
-            }
-#endif
-        }
-        
+       
 
         public static RazorStringHostContainer RazorHost
         {
